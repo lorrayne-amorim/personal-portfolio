2c5548e608459cb5ef4583b545daacfd179993f7
lorrayne-amorim <lorrayne_afdc@hotmail.com>
2025-07-06 11:56:06 -0300

    feat: add PhraseFrame component

---
+++ src/components/PhraseFrame.js
@@ -0,0 +1,12 @@
+import { Phrases } from "./Phrases";
+import { Col } from "react-bootstrap";
+
+export const PhraseFrame = () => {
+    return (
+        <Col lg={12}>
+            <div >
+                <Phrases />
+            </div>
+        </Col>
+    );
+};
